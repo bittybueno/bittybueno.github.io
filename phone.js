2cea7b2492f405a4821441bd1106b707e673fab4
Marissa Bueno <marissa.bueno21@gmail.com>
2019-02-06 16:44:10 -0700

    rename tab

--- phone.js
+++ phone.js
@@ -4,20 +4,28 @@ $(document).ready(function() { // do this when the document is loaded
   $("#contact_list").hide(); 
 });
 
-$("#button1").click(function() { // when "button_id" is clicked
+$("#tab_dialer").click(function() { // when "button_id" is clicked
   $("#dialer").show(); // show element
   $("#contact_list").hide();  // hide other element
   $("#add_contact").hide(); 
 });
 
-$("#button2").click(function() { // when "button_id" is clicked
+$("#tab_contacts").click(function() { // when "button_id" is clicked
   $("#contact_list").show(); // show element
   $("#dialer").hide();  // hide other element
   $("#add_contact").hide(); 
 });
 
-$("#button3").click(function() { // when "button_id" is clicked
+$("#tab_add_contact").click(function() { // when "button_id" is clicked
   $("#add_contact").show(); // show element
   $("#dialer").hide();  // hide other element
   $("#contact_list").hide();  
 });
+
+$("#dialer_pad button").click(function() {
+  $("#number_input").val($("#number_input").val() + this.innerText);
+})
+
+$("#button_dialer_clear").click(function() {
+  $("#number_input").val("");
+})
